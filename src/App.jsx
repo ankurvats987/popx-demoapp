27d1e311cb740b9ff36056f6dcfa906e70e1e3b2
ANKUR <rockingjohnny2004@gmail.com>
2025-06-14 02:39:04 +0530

    upload main files

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./css/App.css";
+import Home from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router";
+
+const Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route path="/create-account" element={<SignUp />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/profile" element={<Profile />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={Router} />;
+}
+
+export default App;
